43a28214af3daa5ea4ec057e475073376008d4ec
norajs <norajsinger@gmail.com>
2017-09-22 03:07:55 -0400

    Added About page

--- about.php
+++ about.php
@@ -7,10 +7,43 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+      <link rel="stylesheet" href="style_about.css">
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
+      
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-xl" id="text">
+                 <p id="title">About Charlie SerabiaN</p>
+                  
+                  <p>Hey there!  It's a pleasure to meet you!</p>
+                  <p>My name's Charlie. I'm an illustrator and fantasy writer who resides in ft lauderdale, fl.</p>
+                  <p>I'm an avid reader, martial artist, and lover of all art forms.</p>
+                  </br>
+                  
+                  
+                  <p id="pricing">pricing</p>
+                  
+                  <p>Because each piece of artwork is different,</p>
+                  <p>pricing varies on each project. i would</p>
+                  <p>prefer to stay flexible for clients. please</p>
+                  <p>send me as much detail as you can for the</p> 
+                  <p>most accurate quote.</p>
+              </div>
+              <div class="col-xl " id="profile">
+                  <img class="img img-fluid" src="pics/profile1.png">
+              </div>
+          
+          </div>
+      
+      </div>
+      
+      
+      
+      
+      
+      
+    
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
