628c94ae223a84b9013af32efa9758acd5353489
norajs <norajsinger@gmail.com>
2017-09-24 23:31:54 -0400

    Writing page done

--- writing.php
+++ writing.php
@@ -7,9 +7,42 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="style_writing.css">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+      
+      
+      <div class="container">
+          <div class="row justify-content-center" >
+            <div class="col-xs align-self-center">
+                <p id="title">
+                    MemoriaS
+                    </br>
+                    <p id="title2">Deep in the ArnakS</p>
+                </p>
+                
+            </div>
+        </div>
+
+        <div class="row middle justify-content-left" >
+            <div class="col-xs col-sm-8 col-md-8 col-lg-8 col-xl-8" id="title3">
+                <p>What makes us? What forms us?</p>
+                <p>Tell me what you see,</p>
+                <p>when you look back?</p>
+                <p>Do you see who you are?</p>
+                <p>Or nothing at all?</p>
+                </br>
+                <a id="shop" href="https://www.amazon.com/Memorias-Deep-Arnaks-Charles-Serabian-ebook/dp/B01BUSIONA"><p>Purchase Vol. 1 here</p></a >
+            </div>
+            
+        </div>
+          
+        
+          
+        
+      
+      </div>
+    
 
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
