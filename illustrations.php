f4931a31f518a2a2c89570c0fa711f4466a510a6
Nora J. Singer <norajs@users.noreply.github.com>
2017-10-12 22:25:06 -0400

    Illustration page done and minor font-size fixes

--- illustrations.php
+++ illustrations.php
@@ -7,9 +7,218 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+      <link rel="stylesheet" href="style_illustrations.css">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+      
+      <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+
+                <a href="/w3images/nature.jpg">
+                    <img class="img-fluid" src="pics/Gallery/AlienOGColor.jpg">
+
+                </a>
+            
+            </div>
+              
+            <div class="col-md-4">
+            
+              <a href="/w3images/nature.jpg">
+                <img class="img-fluid" src="pics/Gallery/BananaSplitcolor4.jpg">
+                
+              </a>
+            
+            </div>
+            <div class="col-md-4">
+           
+              <a href="/w3images/fjords.jpg">
+                <img class="img-fluid" src="pics/Gallery/GreenRebellionFINAL2.jpg">
+                
+              </a>
+            
+            </div>
+        </div>
+          
+        <div class="row">
+            <div class="col-md-4">
+
+                <a href="/w3images/nature.jpg">
+                    <img class="img-fluid" src="pics/Gallery/Dragon.jpg">
+
+                </a>
+            
+            </div>
+              
+            <div class="col-md-4">
+            
+              <a href="/w3images/nature.jpg">
+                <img class="img-fluid" src="pics/Gallery/flyingrats.jpg">
+                
+              </a>
+            
+            </div>
+            <div class="col-md-4">
+           
+              <a href="/w3images/fjords.jpg">
+                <img class="img-fluid" src="pics/Gallery/NamelessThing.jpg">
+                
+              </a>
+            
+            </div>
+        </div>
+          
+        <div class="row">
+            <div class="col-md-6">
+
+                <a href="/w3images/nature.jpg">
+                    <img class="img-fluid" src="pics/Gallery/autumnland.jpg">
+
+                </a>
+            
+            </div>
+              
+            <div class="col-md-6">
+            
+              <a href="/w3images/nature.jpg">
+                <img class="img-fluid" src="pics/Gallery/GuardingtheCave.jpg">
+                
+              </a>
+            
+            </div>
+            
+        </div>
+          
+        <div class="row">
+            <div class="col-md-4">
+
+                <a href="/w3images/nature.jpg">
+                    <img class="img-fluid" src="pics/Gallery/IMG_7523.jpg">
+
+                </a>
+            
+            </div>
+              
+            <div class="col-md-4">
+            
+              <a href="/w3images/nature.jpg">
+                <img class="img-fluid" src="pics/Gallery/Pose1.jpg">
+                
+              </a>
+            
+            </div>
+            <div class="col-md-4">
+           
+              <a href="/w3images/fjords.jpg">
+                <img class="img-fluid" src="pics/Gallery/Raider.jpeg">
+                
+              </a>
+            
+            </div>
+        </div>
+          
+         <div class="row">
+            <div class="col-md-6">
+
+                <a href="/w3images/nature.jpg">
+                    <img class="img-fluid" src="pics/Gallery/Rancor.jpg">
+
+                </a>
+            
+            </div>
+              
+            <div class="col-md-6">
+            
+              <a href="/w3images/nature.jpg">
+                <img class="img-fluid" src="pics/Gallery/TheDM.jpg">
+                
+              </a>
+            
+            </div>
+            
+        </div>
+          
+        <div class="row">
+            <div class="col-md-6">
+
+                <a href="/w3images/nature.jpg">
+                    <img class="img-fluid" src="pics/Gallery/Ahimka.jpeg">
+
+                </a>
+            
+            </div>
+              
+            <div class="col-md-6">
+            
+              <a href="/w3images/nature.jpg">
+                <img class="img-fluid" src="pics/Gallery/thefish.jpg">
+                
+              </a>
+            
+            </div>
+            
+        </div>
+          
+        <div class="row">
+            <div class="col-md-6">
+
+                <a href="/w3images/nature.jpg">
+                    <img class="img-fluid" src="pics/Gallery/rozv.jpg">
+
+                </a>
+            
+            </div>
+              
+            <div class="col-md-6">
+            
+              <a href="/w3images/nature.jpg">
+                <img class="img-fluid" src="pics/Gallery/vecq.jpg">
+                
+              </a>
+            
+            </div>
+            
+        </div>
+          
+        <div class="row">
+            <div class="col-md-6">
+
+                <a href="/w3images/nature.jpg">
+                    <img class="img-fluid" src="pics/Gallery/ocnipj.jpg">
+
+                </a>
+            
+            </div>
+              
+            <div class="col-md-6">
+            
+              <a href="/w3images/nature.jpg">
+                <img class="img-fluid" src="pics/Gallery/op.jpg">
+                
+              </a>
+            
+            </div>
+            
+        </div>
+      </div>
+      
+    
+      
+      
+      
+      
+      
+      
+      
+      
+
+      
+      
+      
+      
+      
+      
+      
 
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
--- more.php
+++ more.php
@@ -11,11 +11,41 @@
   </head>
   <body>
     <div class="container">
-        <div class="row">
+          <div class="row first justify-content-center" >
+            <div class="col-xs align-self-center">
+                <p id="title">
+                    MemoriaS
+                    </br>
+                    <p id="title2">Deep in the ArnakS</p>
+                </p>
+                
+            </div>
         </div>
+
+        <div class="row middle justify-content-left" >
+            <div class="col-xs col-sm-8 col-md-8 col-lg-8 col-xl-8 " id="title3">
+                <p>Trained by Loboza and the captive human</p>
+                <p>knight, Jerryl Trought, they have grown</p>
+                <p>smart and skilled, in measure enough to</p>
+                <p>attempt escape, though the goal eludes them.</p>
+                <p>From the northeast, a stranger approaches,</p>
+                <p>carrying with him a mission that will change</p>
+                <p>the Arnaks forever.</p>
+                <p>Five lives interwined, five minds that will be</p>
+                <p>forever changed. Though all will wonder</p>
+                <p>by the end what it is they truly want...</p>
+               
+                
+               
+            </div>
+            
+        </div>
+          
+        
+          
+        
       
-      
-    </div>
+      </div>
 
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
